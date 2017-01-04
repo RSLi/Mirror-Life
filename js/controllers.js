ab64355f0fa069ffc73642b97136cb92a9f3e361
RSLi <robertsli@163.com>
2017-01-04 00:54:14 -0500

    Completed all home screen functionalities

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,39 @@ myApp.controllers = {
         page.querySelector('#btn-splitter-toggle').onclick = function() {
             document.querySelector('#mySplitter').left.toggle();
         }
+
+        page.querySelector('#btn-increase-point').onclick = function() {
+            var changeAmount = 1 * myApp.models.user.getMultiplier();
+            myApp.models.user.increasePoint(changeAmount);
+            myApp.views.homePage.render(page);
+        }
+
+        page.querySelector('#btn-decrease-point').onclick = function() {
+            var changeAmount = -1;
+            myApp.models.user.increasePoint(changeAmount);
+            myApp.views.homePage.render(page);
+        }
+
+        page.querySelector('#btn-increase-multiplier').onclick = function() {
+            var original = parseInt(myApp.models.user.getMultiplier());
+            var result = original + 1;
+            if (result > 3){
+                result = 3;
+            }
+            myApp.models.user.setMultiplier(result);
+            myApp.views.homePage.render(page);
+        }
+
+        page.querySelector('#btn-decrease-multiplier').onclick = function() {
+            var original = parseInt(myApp.models.user.getMultiplier());
+            var result = original - 1;
+            if (result < 1){
+                result = 1;
+            }
+            myApp.models.user.setMultiplier(result);
+            myApp.views.homePage.render(page);
+        }
+
+        myApp.views.homePage.render(page);
     }
 }
---
+++ js/views.js
@@ -0,0 +1,48 @@
+myApp.views = {
+    homePage: {
+        render: function(page) {
+            var point = myApp.models.user.getPoint();
+            var today_point = myApp.models.user.getTodayPoint();
+            var multiplier = myApp.models.user.getMultiplier();
+            var hour = new Date().getHours();
+
+            page.querySelector('#field-level').innerHTML = Math.floor(point / 10);
+            page.querySelector('#field-exp').value = point % 10 * 10;
+            page.querySelector('#field-total-point').innerHTML = point;
+            page.querySelector('#field-total-today').innerHTML = today_point;
+
+            //Rendering Achievement Icon (badge)
+            if (point < 500) {
+                page.querySelector('#field-icon-achievement').setAttribute("icon", "ion-university");
+            } else if (point < 1000) {
+                page.querySelector('#field-icon-achievement').setAttribute("icon", "ion-ribbon-a");
+            } else {
+                page.querySelector('#field-icon-achievement').setAttribute("icon", "ion-trophy");
+            }
+
+            //Rendering Stamina Icon
+            var iconStamina = page.querySelector('#field-icon-stamina');
+            if (hour >= 23) {
+                iconStamina.setAttribute("icon", "ion-battery-empty");
+            } else if (hour >= 18) {
+                iconStamina.setAttribute("icon", "ion-battery-low");
+            } else if (hour >= 13) {
+                iconStamina.setAttribute("icon", "ion-battery-half");
+            } else if (hour >= 7) {
+                iconStamina.setAttribute("icon", "ion-battery-full");
+            } else {
+                iconStamina.setAttribute("icon", "ion-battery-charging");
+            }
+
+            //Rendering Multiplier Icon
+            var iconMultiplier = page.querySelector('#field-icon-multiplier');
+            if (multiplier == 1) {
+                iconMultiplier.setAttribute("icon", "ion-volume-low");
+            } else if (multiplier == 2) {
+                iconMultiplier.setAttribute("icon", "ion-volume-medium");
+            } else {
+                iconMultiplier.setAttribute("icon", "ion-volume-high");
+            }
+        }
+    }
+}
